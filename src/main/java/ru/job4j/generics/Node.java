f99c4521b162e5d631130069e653d52e62dc4f99
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-01-16 00:58:57 +0200

    job4j_design

---
+++ src/main/java/ru/job4j/generics/Node.java
@@ -0,0 +1,18 @@
+package ru.job4j.generics;
+
+/**
+ * @author Artem Polshchak on 23.12.2021.
+ * @project job4j_design 0. Что такое обобщенные типы (generics) [#4952]
+ */
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    public Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+    public T getData() {
+        return data;
+    }
+}
