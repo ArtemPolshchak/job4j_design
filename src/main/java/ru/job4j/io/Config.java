d0573925690230d6e00983f52b39927f9fca86af
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-02-04 03:56:04 +0200

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,83 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * @author Artem Polshchak on 03.02.2022.
+ * @project job4j_design
+ * 1. Читаем файл конфигурации [#858]
+ * Уровень : 2. ДжуниорКатегория : 2.2. Ввод-выводТопик : 2.2.1. Ввод-вывод
+ */
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    /**
+     * Метод добавляет инфорвацию с файла .properties в config -> key, value
+     * метод игнорирует комментарии
+     * метод игнорирует пустые строки
+     */
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            while (reader.ready()) {
+                String str = reader.readLine();
+                if (str.isEmpty() || str.contains("#") || !str.contains("=")) {
+                    continue;
+                } else {
+                    String[] strings = str.split("=");
+                    if (strings.length == 2 && (!strings[0].isEmpty() && !strings[1].isEmpty())) {
+                        values.put(strings[0], strings[1]);
+                    } else {
+                        throw new IllegalArgumentException();
+
+                    }
+
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Метод производит поиск значения в config  по ключу
+     * @param key ключ, по которому осуществляется поиск
+     * @return entry.getValue()
+     */
+    public String value(String key) {
+        String res = "";
+        if (values.containsKey(key)) {
+            for (Map.Entry<String, String> entry : values.entrySet()) {
+                if (entry.getKey().equals(key)) {
+                    res =  entry.getValue();
+                }
+            }
+        } else {
+            return null;
+        }
+        return res;
+    }
+
+    /**
+     * Переопределенный метод, выводит на экран содержимоее config
+     * @return StringJoiner out
+     */
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        for (Map.Entry<String, String> entry : values.entrySet()) {
+            out.add(entry.getKey() + "=" + entry.getValue());
+        }
+        return out.toString();
+    }
+}
---
+++ src/test/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Artem Polshchak on 03.02.2022.
+ * @project job4j_design
+ * 1. Читаем файл конфигурации [#858]
+ * Уровень : 2. ДжуниорКатегория : 2.2. Ввод-выводТопик : 2.2.1. Ввод-вывод
+ */
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr Arsentev"));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPairThroughIllegalArgException() {
+        String path = "./data/pair_with_IllegalArgException.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertFalse(config.toString().contains("# PostgreSQL"));
+
+    }
+
+    @Test
+    public void whenPairWithEmptyLines() {
+        String path = "./data/pair_with_empty_lines.properties";
+        Config config = new Config(path);
+        config.load();
+        String str = config.toString();
+        assertThat(str, is("fileName=www.coll\r\nname=Petr Arsentev\r\nPathName=d.call.to\r\nsecondName=Vitas"));
+    }
+}
