3e80e9dfdcd78a1e20b3f3bd5e9f01fb8e7b1938
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-02-06 04:38:19 +0200

    3.0. Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+
+
+/**
+ * @author Artem Polshchak on 06.02.2022.
+ * @project job4j_design
+ * 3.0. Тестирование IO [#173905]
+ * Уровень : 2. ДжуниорКатегория : 2.2. Ввод-выводТопик : 2.2.1. Ввод-вывод
+ */
+
+public class AnalysisTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void drop() throws IOException {
+        File source = folder.newFile("server.csv");
+        File target = folder.newFile("target.csv");
+
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("400 10:58:01");
+            out.println("500 12:58:01");
+            out.println("200 15:57:01");
+        }
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+
+        assertThat(rsl.toString(), is("10:58:01;15:57:01"));
+    }
+}
