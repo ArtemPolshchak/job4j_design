3c15ded89f5c7df489c860ba89bde962faf3ecf8
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-02-07 03:53:38 +0200

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+/**
+ * @author Artem Polshchak on 06.02.2022.
+ * @project job4j_design
+ * 4.0. File [#252491]
+ * Уровень : 2. ДжуниорКатегория : 2.2. Ввод-выводТопик : 2.2.1. Ввод-вывод
+ */
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getName() + " " + subfile.length());
+        }
+    }
+}
