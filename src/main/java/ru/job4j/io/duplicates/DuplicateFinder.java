9b9f33c8796a8b5c77078b9de8c86ef7258a490f
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-02-10 02:59:07 +0200

    4.2. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicateFinder.java
@@ -0,0 +1,17 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * @author Artem Polshchak on 09.02.2022.
+ * @project job4j_design 4.2. Поиск дубликатов [#315066]
+ * Уровень : 2. ДжуниорКатегория : 2.2. Ввод-выводТопик : 2.2.1. Ввод-вывод
+ */
+
+public class DuplicateFinder {
+    public static void main(String[] args) throws IOException {
+        Files.walkFileTree(Path.of("D:\\Новая Папка"), new DuplicatesVisitor());
+    }
+}
---
+++ src/main/java/ru/job4j/io/duplicates/DuplicateVisitor.java
@@ -0,0 +1,30 @@
+package ru.job4j.io.duplicates;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+/**
+ * @author Artem Polshchak on 09.02.2022.
+ * @project job4j_design 4.2. Поиск дубликатов [#315066]
+ * Уровень : 2. ДжуниорКатегория : 2.2. Ввод-выводТопик : 2.2.1. Ввод-вывод
+ */
+
+class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private final HashSet<FileProperty> set = new HashSet<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
+        if (set.contains(fileProperty)) {
+            System.out.println("Дубликат " + file);
+        } else {
+            set.add(fileProperty);
+        }
+        return CONTINUE;
+    }
+}
