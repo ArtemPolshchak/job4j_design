59893654287894793920466e373d948ef1ef7b64
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-02-12 17:08:17 +0200

    5.1. Именованные аргументы [#295518 #263677]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -27,8 +27,11 @@ public class ArgsName {
                 if (list[1].equals("")) {
                     throw new IllegalArgumentException("there is incorrect value of key");
                 } else {
-                    String key = list[0].substring(1);
-                    values.put(key, list[1]);
+                    if (list[0].startsWith("-")) {
+                        String key = list[0].substring(1);
+                        values.put(key, list[1]);
+                    }
+                    values.put(list[0], list[1]);
                 }
         }
     }
@@ -40,7 +43,7 @@ public class ArgsName {
     }
 
     public static void main(String[] args) {
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        ArgsName jvm = ArgsName.of(new String[] {"Xmx=512", "-encoding=UTF-8"});
         System.out.println(jvm.get("Xmx"));
 
         ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
