1d80cffbf3650c03e002e034c617e383fbb748cf
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-02-12 03:37:31 +0200

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -16,8 +16,11 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("D:\\testForJob4j");
-        search(start, path -> path.toFile().getName().endsWith(".xml")).forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
 
     }
 
