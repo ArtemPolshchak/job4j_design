381a9c1b125c3918ea41ddddfa4f8eb91d654110
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-02-23 00:14:59 +0200

    job4j

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+/**
+ * @author Artem.polschak@gmail.com on 18.02.2022.
+ * @project job4j_design
+ * 7. Scanner [#504791]
+ * Уровень : 2. ДжуниорКатегория : 2.2. Ввод-выводТопик : 2.2.1. Ввод-вывод
+ */
+public class CSVReader {
+    public static void handle(ArgsName argsName) throws Exception {
+
+    }
+}
---
+++ src/test/java/ru/job4j/io/CSVReaderTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.io;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.nio.file.Files;
+
+/**
+ * @author Artem.polschak@gmail.com on 18.02.2022.
+ * @project job4j_design
+ * 7. Scanner [#504791]
+ * Уровень : 2. ДжуниорКатегория : 2.2. Ввод-выводТопик : 2.2.1. Ввод-вывод
+ */
+public class CSVReaderTest {
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Test
+    public void whenFilterTwoColumns() throws Exception {
+        String data = String.join(
+                System.lineSeparator(),
+                "name;age;last_name;education",
+                "Tom;20;Smith;Bachelor",
+                "Jack;25;Johnson;Undergraduate",
+                "William;30;Brown;Secondary special"
+        );
+        File file = temporaryFolder.newFile("source.cvs");
+        File target = temporaryFolder.newFile("target.cvs");
+        ArgsName argsName = ArgsName.of(new String[]{
+                "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(), "-filter=name,age"
+        });
+        Files.writeString(file.toPath(), data);
+        String expected = String.join(
+                System.lineSeparator(),
+                "name;age",
+                "Tom;20",
+                "Jack;25",
+                "William;30"
+        ).concat(System.lineSeparator());
+        CSVReader.handle(argsName);
+        Assert.assertEquals(expected, Files.readString(target.toPath()));
+    }
+}
