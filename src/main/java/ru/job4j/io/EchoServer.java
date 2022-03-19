99198e0b942cde22e353b94d2e2a64238a4bea57
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-03-19 23:27:55 +0200

    2. Что такое Socket? [#4850]

---
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -0,0 +1,42 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author artem.polschak@gmail.com on 19.03.2022.
+ * @project job4j_design
+ * 2.Socket [#4850]
+ * Уровень : 2. ДжуниорКатегория : 2.2. Ввод-выводТопик : 2.2.2. Socket
+ */
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (!server.isClosed()) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(socket.getInputStream()))) {
+
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String bye = "Bye";
+                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                        System.out.println(str);
+                        if (str.contains(bye)) {
+                            String[] strings = str.split("=");
+                            String[] secondStrings = strings[1].split(" ");
+                            if (secondStrings[0].equals(bye)) {
+                                server.close();
+                            }
+                        }
+                    }
+                    out.flush();
+                }
+            }
+        }
+    }
+}
