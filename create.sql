2909f2fd4a1868bfc2aa0ebc5a458bc203c444ae
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-04-30 02:53:59 +0300

    1. Схема прав пользователи и роли. [#1093 #294554]

--- create.sql
+++ create.sql
@@ -1,55 +1,56 @@
-CREATE TABLE users (
-user_id serial primary key,
-name character varying(100),
-phone character varying(15)
-);
-
-CREATE TABLE item (
-item_id serial primary key,
-item_name character varying(100),
-item_date timestamp not null default now()
-);
-
-CREATE TABLE user_item (
-item_id int REFERENCES item(item_id),
-user_id int REFERENCES users(user_id)
-);
-
 CREATE TABLE role (
-role_type character varying(20) primary key,
-user_id int REFERENCES users(user_id),
+id serial primary key,
+role_type character varying(20),
 role_description character varying(160)
 );
 
 CREATE TABLE rule (
-rule_type character varying(20) primary key,
+id serial primary key,
+rule_type character varying(20),
 rule_description character varying(160)
 );
 
-CREATE TABLE role_rule (
-role_type character varying(20) REFERENCES role(role_type),
-rule_type character varying(20) REFERENCES rule(rule_type)
-);
-
 CREATE TABLE category (
-category_name character varying(20),
-item_id int REFERENCES item(item_id)
+id serial primary key,
+category_name character varying(20)
 );
 
-CREATE TABLE status (
-status_name character varying(20),
-item_id int REFERENCES item(item_id)
+CREATE TABLE state (
+id serial primary key,
+status_name character varying(20)
 );
+
+CREATE TABLE role_rule (
+role_id  int REFERENCES role(id),
+rule_id  int REFERENCES rule(id)
+);
+
+CREATE TABLE users (
+id serial primary key,
+login character varying(20),
+password character varying(15),
+role_id int REFERENCES role(id)
+);
+
+CREATE TABLE item (
+id serial primary key,
+item_name character varying(100),
+item_date timestamp not null default now(),
+users_id int REFERENCES users(id),
+category_id int REFERENCES category(id),
+state_id int REFERENCES state(id)
+);
+
 
 CREATE TABLE comment (
-comment_id serial primary key,
-item_id int REFERENCES item(item_id),
+id serial primary key,
+item_id int REFERENCES item(id),
 comment_text character varying(6000),
 comment_date timestamp not null default now()
 );
 
 CREATE TABLE attach (
-attach_id serial primary key,
-item_id int REFERENCES item(item_id),
+id serial primary key,
+item_id int REFERENCES item(id),
 attach_file bytea
 );
--- insert.sql
+++ insert.sql
@@ -1,14 +1,17 @@
-INSERT INTO users (name, phone) values ('Nick Frolov', '+00099999999');
-INSERT INTO users (name, phone) values ('Dan Balan', '+00009111111');
+INSERT INTO role (role_type) values ('admin');
+INSERT INTO role (role_type) values ('user');
 
-INSERT INTO item (item_name) values ('bug#1');
-INSERT INTO item (item_name) values ('bug#2');
-INSERT INTO item (item_name) values ('bug#3');
+INSERT INTO state (status_name) values ('accepted');
+INSERT INTO state (status_name) values ('awaiting');
+
+INSERT INTO item (id, item_name) values ('1','bug#1');
+INSERT INTO item (id, item_name) values ('2', 'bug#2');
+INSERT INTO item (id, item_name) values ('3', 'bug#3');
 
 INSERT INTO comment (item_id, comment_text) values ('1', 'comment#1');
 
-INSERT INTO role (role_type, user_id) values ('user', '1');
-INSERT INTO role (role_type, user_id) values ('admin', '2');
+INSERT INTO users (login, password, role_type) values ('Nick_Frolov', 'qwerty1', 'admin');
+INSERT INTO users (login, password, role_type) values ('Dan_Balan', 'qwerty2', 'user');
 
 INSERT INTO rule (rule_type) values ('admin_rules');
 INSERT INTO rule (rule_type) values ('user_rules');
@@ -16,15 +19,12 @@ INSERT INTO rule (rule_type) values ('user_rules');
 INSERT INTO role_rule (role_type, rule_type) values ('user', 'user_rules');
 INSERT INTO role_rule (role_type, rule_type) values ('admin', 'admin_rules');
 
-INSERT INTO category (category_name, item_id) values ('bugs', '1');
-INSERT INTO category (category_name, item_id) values ('bugs', '2');
-INSERT INTO category (category_name, item_id) values ('bugs', '3');
+INSERT INTO category (category_name) values ('bugs1');
+INSERT INTO category (category_name) values ('bugs2');
+INSERT INTO category (category_name) values ('bugs3');
 
 INSERT INTO category (category_name) values ('offers');
 INSERT INTO category (category_name) values ('info');
 
-INSERT INTO status (status_name, item_id) values ('accepted', '1');
-INSERT INTO status (status_name, item_id) values ('awaiting', '2');
-INSERT INTO status (status_name, item_id) values ('awaiting', '3');
-
-INSERT INTO status (status_name) values ('completed');
+INSERT INTO state (status_name) values ('accepted');
+INSERT INTO state (status_name) values ('awaiting');
