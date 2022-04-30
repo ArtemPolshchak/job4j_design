d2046809c8817e5e58dbff14a8d75eaa1863f487
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-04-30 23:47:09 +0300

    1. Схема прав пользователи и роли. [#1093 #294554]

--- insert.sql
+++ insert.sql
@@ -1,30 +1,29 @@
-INSERT INTO role (role_type) values ('admin');
-INSERT INTO role (role_type) values ('user');
+INSERT INTO role (id, role_type) values ('1', 'admin');
+INSERT INTO role (id, role_type) values ('2', 'user');
 
-INSERT INTO state (status_name) values ('accepted');
-INSERT INTO state (status_name) values ('awaiting');
+INSERT INTO rule (id, rule_type) values ('1', 'admin_rules');
+INSERT INTO rule (id, rule_type) values ('2', 'user_rules');
 
-INSERT INTO item (id, item_name) values ('1','bug#1');
-INSERT INTO item (id, item_name) values ('2', 'bug#2');
-INSERT INTO item (id, item_name) values ('3', 'bug#3');
+INSERT INTO role_rule (role_id, rule_id) values ('1', '1');
+INSERT INTO role_rule (role_id, rule_id) values ('2', '2');
 
-INSERT INTO comment (item_id, comment_text) values ('1', 'comment#1');
+INSERT INTO state (id, status_name) values ('1', 'accepted');
+INSERT INTO state (id, status_name) values ('2', 'awaiting');
+INSERT INTO state (id, status_name) values ('3', 'complited');
 
-INSERT INTO users (login, password, role_type) values ('Nick_Frolov', 'qwerty1', 'admin');
-INSERT INTO users (login, password, role_type) values ('Dan_Balan', 'qwerty2', 'user');
+INSERT INTO category (id, category_name) values ('1', 'bugs1');
+INSERT INTO category (id, category_name) values ('2', 'bugs2');
+INSERT INTO category (id, category_name) values ('3', 'bugs3');
 
-INSERT INTO rule (rule_type) values ('admin_rules');
-INSERT INTO rule (rule_type) values ('user_rules');
+INSERT INTO category (id, category_name) values ('4', 'offers');
+INSERT INTO category (id, category_name) values ('5', 'info');
 
-INSERT INTO role_rule (role_type, rule_type) values ('user', 'user_rules');
-INSERT INTO role_rule (role_type, rule_type) values ('admin', 'admin_rules');
 
-INSERT INTO category (category_name) values ('bugs1');
-INSERT INTO category (category_name) values ('bugs2');
-INSERT INTO category (category_name) values ('bugs3');
+INSERT INTO users (id, login, password, role_id) values ('1', 'Nick_Frolov', 'qwerty1', '1');
+INSERT INTO users (id, login, password, role_id) values ('2', 'Dan_Balan', 'qwerty2', '2');
 
-INSERT INTO category (category_name) values ('offers');
-INSERT INTO category (category_name) values ('info');
+INSERT INTO item (id, item_name, users_id, category_id, state_id) values ('1','bug#1', '1', '1', '2');
+INSERT INTO item (id, item_name, users_id, category_id, state_id) values ('2','bug#2', '2', '5', '3');
+INSERT INTO item (id, item_name, users_id, category_id, state_id) values ('3', 'bug#3', '2', '1', '1');
 
-INSERT INTO state (status_name) values ('accepted');
-INSERT INTO state (status_name) values ('awaiting');
+INSERT INTO comment (id, item_id, comment_text) values ('1', '1', 'comment#1');
