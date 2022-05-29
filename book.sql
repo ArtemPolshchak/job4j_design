b687bd7279d9dfe5f7da195e0d0038b82b4eaf06
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-05-30 00:24:44 +0300

    1. Inner join [#373617]

---
+++ book.sql
@@ -0,0 +1,28 @@
+CREATE TABLE supply (
+    id SERIAL PRIMARY KEY,
+    price DECIMAL(8, 2),
+    amount INT
+    );
+
+CREATE TABLE book (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50),
+    author VARCHAR(50),
+    price_id INT REFERENCES supply(id)
+    );
+   
+    
+INSERT INTO supply (price, amount) VALUES (518.99, 2);
+INSERT INTO supply (price, amount) VALUES (570.20, 6);
+INSERT INTO supply (price, amount) VALUES (540.50, 7);
+INSERT INTO supply (price, amount) VALUES (360.80, 3);
+    
+INSERT INTO book (title, author, price_id) VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 1);
+INSERT INTO book (title, author, price_id) VALUES ('Белая гвардия', 'Булгаков М.А.', 2);
+INSERT INTO book (title, author, price_id) VALUES ('Идиот', 'Достоевский Ф.М.', 3);
+INSERT INTO book (title, author, price_id) VALUES ('Братья Карамазовы', 'Достоевский Ф.М.', 4);
+INSERT INTO book (title, author) VALUES ('Стихотворения и поэмы', 'Есенин С.А.');
+
+     SELECT * FROM book b INNER
+     JOIN supply s
+     ON b.price_id = s.id;
