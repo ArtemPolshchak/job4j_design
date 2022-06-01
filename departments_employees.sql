fc6c98515be8d28becad777c3f8ad38cdf992e5f
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-06-02 02:54:26 +0300

    0. Join [#6862 #307732]

--- departments_employees.sql
+++ departments_employees.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE departments (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50)
@@ -10,37 +11,35 @@ CREATE TABLE employees (
     );
     
 INSERT INTO departments (name) 
-VALUES ('surgery'), ('gynaecology'), ('obstetrics'), ('paediatrics'), ('eye'), ('ENT'),
-('dental'), ('orthopaedics'), ('neurology'), ('cardiology'), ('psychiatry'), ('skin'), ('plastic surgery'), ('nuclear medicine'), ('infectious disease');
+VALUES ('surgery'), ('gynaecology'), 
+('obstetrics'), ('paediatrics'), 
+('eye'), ('ENT'),
+('dental'), ('orthopaedics'), 
+('neurology'), ('cardiology'), 
+('psychiatry'), ('skin'), 
+('plastic surgery'), ('nuclear medicine'), 
+('infectious disease');
 
 INSERT INTO employees (name, departments_id) 
-VALUES ('Стройников И.С.', 1), ('Борисенко Д.С.', 1), ('Странова М.Н.', 3), ('Любенко Д.С.', 5), 
-('Костенко Н.А.', 7), ('Кучеренко С.Л', 9), ('Валентинова И.Д', 11);
+VALUES ('Стройников И.С.', 1), ('Борисенко Д.С.', 1), 
+('Странова М.Н.', 3), ('Любенко Д.С.', 5), 
+('Костенко Н.А.', 7), ('Кучеренко С.Л', 9), 
+('Валентинова И.Д', 11);
 
-SELECT * FROM departments;
-SELECT * FROM employees;
-
-SELECT e.name, d.name FROM employees e LEFT JOIN departments d
-ON e.departments_id = d.id;
-
-SELECT e.name, d.name FROM departments d RIGHT JOIN employee e
-ON d.id = e.departments_id;
-
-SELECT e.name, d.name FROM employees e RIGHT JOIN departments d
-ON e.departments_id = d.id
-WHERE e.name IS NOT NULL;
- 
-SELECT e.name, d.name FROM employees e RIGHT JOIN departments d
-ON e.departments_id = d.id;
-
+--2
+SELECT e.name, d.name FROM employees e LEFT JOIN departments d ON e.departments_id = d.id;
+SELECT e.name, d.name FROM employees e RIGHT JOIN departments d ON e.departments_id = d.id;
+SELECT * FROM employees FULL JOIN departments;
 SELECT e.name, d.name FROM employees e CROSS JOIN departments d;
 
-SELECT * FROM employees CROSS JOIN departments;
+--3
+SELECT DISTINCT e.name AS 'сотрудник',  d.name AS 'департамент' FROM employees e RIGHT JOIN departments d
+ON e.departments_id = d.id
+WHERE e.name IS NULL
+ORDER BY e.name;
 
+--4
 SELECT e.name, d.name FROM employees e LEFT JOIN departments d
-ON e.departments_id = d.id
-WHERE d.name IS NULL;
-
-SELECT d.name, e.name FROM departments d LEFT JOIN employees e
-ON e.departments_id = d.id
-WHERE e.name IS NULL;
+ON e.departments_id = d.id;
+SELECT e.name, d.name FROM departments d RIGHT JOIN employees e
+ON d.id = e.departments_id;
--- teens.sql
+++ teens.sql
@@ -1,19 +1,11 @@
-CREATE TABLE gender (
-    id SERIAL PRIMARY KEY,
-    gender VARCHAR(50)
-    );
-    
-    CREATE TABLE teens (
+CREATE TABLE gender_teens (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50),
-    gender_id INT REFERENCES gender(id)
+    gender VARCHAR(50)
     );
-    
-INSERT INTO gender (gender) 
-VALUES ('male'), ('female');
+   
+INSERT INTO gender_teens (name, gender) 
+VALUES ('мальчик 1', 'male'), ('девочка 1', 'female'), ('мальчик 2', 'male'), ('мальчик 3', 'male'), 
+('мальчик 4', 'male'), ('девочка 2', 'female'), ('девочка 3', 'female');
 
-INSERT INTO teens (name, gender_id) 
-VALUES ('мальчик 1', 1), ('девочка 1', 2), ('мальчик 2', 1), ('мальчик 3', 1), 
-('мальчик 4', 1), ('девочка 2', 2), ('девочка 3', 2);
-
-SELECT * FROM teens CROSS JOIN gender;
+SELECT gt.name, gt.gender, g.name, g.gender FROM gender_teens gt CROSS JOIN gender_teens g;
