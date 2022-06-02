fbc47a3af1e5e2b2e6d04cac3fad5d7af27f3800
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-06-03 01:21:43 +0300

    0. Join [#6862 #307732]

--- teens.sql
+++ teens.sql
@@ -8,4 +8,4 @@ INSERT INTO gender_teens (name, gender)
 VALUES ('мальчик 1', 'male'), ('девочка 1', 'female'), ('мальчик 2', 'male'), ('мальчик 3', 'male'), 
 ('мальчик 4', 'male'), ('девочка 2', 'female'), ('девочка 3', 'female');
 
-SELECT gt.name, gt.gender, g.name, g.gender FROM gender_teens gt CROSS JOIN gender_teens g;
+SELECT gt.name, g.name FROM gender_teens gt CROSS JOIN gender_teens g WHERE gt.gender != g.gender;
