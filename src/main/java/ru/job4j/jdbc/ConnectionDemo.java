cce28e7db90a80c6636fa34fd9f630eaca78c42b
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-06-17 01:44:10 +0300

    0. JDBC [#6863 #312948]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -1,23 +1,45 @@
 package ru.job4j.jdbc;
 
 import ru.job4j.io.Config;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 /**
  * @author Artem.polschak@gmail.com on 16.06.2022.
  * @project job4j_design
  * 0. JDBC [#6863]
  */
+
 public class ConnectionDemo {
+    private final Properties prs = new Properties();
+
+    public String getValue(String key) {
+        return this.prs.getProperty(key);
+    }
+
+    public void load(InputStream io) {
+        try {
+            this.prs.load(io);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Config config = new Config("C:\\projects\\job4j_design\\src\\main\\resources\\app.properties");
-        config.load();
+        ConnectionDemo connectionDemo = new ConnectionDemo();
+        ClassLoader loader = ConnectionDemo.class.getClassLoader();
 
-        Class.forName(config.value("hibernate.connection.driver_class"));
-        String url  = config.value("hibernate.connection.url");
-        String login = config.value("hibernate.connection.username");
-        String password = config.value("hibernate.connection.password");
+        try (InputStream io = loader.getResourceAsStream("app.properties")) {
+            connectionDemo.load(io);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Class.forName(connectionDemo.getValue("hibernate.connection.driver_class"));
+        String url  = connectionDemo.getValue("hibernate.connection.url");
+        String login = connectionDemo.getValue("hibernate.connection.username");
+        String password = connectionDemo.getValue("hibernate.connection.password");
 
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
