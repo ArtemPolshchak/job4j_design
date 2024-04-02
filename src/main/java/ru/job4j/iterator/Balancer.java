552addf84c8b26f6610e138ca1cafd11ae126841
Артем Польщак <artem.polschak@gmail.com>
2024-04-02 13:38:04 +0300

    7. Балансир [#505083]

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,18 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        int index = 0;
+        while (source.hasNext()) {
+            nodes.get(index).add(source.next());
+            index++;
+            if (index == nodes.size()) {
+                index = 0;
+            }
+        }
+    }
+}
