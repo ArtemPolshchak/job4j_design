caeeef598086b9f02278bd78c491fbecfe346d26
Артем Польщак <artem.polschak@gmail.com>
2024-06-21 00:45:48 +0200

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+    private int sizeOfEmpty = 10;
+
+    public String surname;
+
+    String name;
+
+    Broken() {
+
+    }
+
+    void echo() { }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d) {
+
+    }
+}
