702fc25cdbfb0e2e4835aadb4069fce90a97eeec
Артем Польщак <artem.polschak@gmail.com>
2024-06-24 14:51:11 +0200

    2. Интерфейс Deque [#504943 #501886]

--- src/main/java/ru/job4j/queue/ReconstructPhrase.java
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -18,9 +18,9 @@ public class ReconstructPhrase {
         int size = evenElements.size();
         for (int i = 0; i < size; i++) {
             if (i % 2 == 0) {
-                builder.append(evenElements.removeFirst());
+                builder.append(evenElements.pollFirst());
             } else {
-                evenElements.removeFirst();
+                evenElements.pollFirst();
             }
         }
         return builder.toString();
